666f502a1da82f4898b549c9f89d27f703a5ff7f
Владислав Кондратенко <Vladkondratencko@gmail.com>
2023-11-12 13:50:17 +0300

    Добавьте файлы проекта.

---
+++ finger/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace finger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.last = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.first = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.last)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.first)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // last
+            // 
+            this.last.BackColor = System.Drawing.Color.Transparent;
+            this.last.Image = global::finger.Properties.Resources.Безымянный_1_09;
+            this.last.Location = new System.Drawing.Point(812, 238);
+            this.last.Name = "last";
+            this.last.Size = new System.Drawing.Size(50, 50);
+            this.last.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.last.TabIndex = 4;
+            this.last.TabStop = false;
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.Transparent;
+            this.p3.Image = global::finger.Properties.Resources.Безымянный_1_06;
+            this.p3.Location = new System.Drawing.Point(747, 249);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(100, 100);
+            this.p3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p3.TabIndex = 3;
+            this.p3.TabStop = false;
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.Transparent;
+            this.p2.Image = global::finger.Properties.Resources.Безымянный_1_03;
+            this.p2.Location = new System.Drawing.Point(669, 275);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(150, 150);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p2.TabIndex = 2;
+            this.p2.TabStop = false;
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Transparent;
+            this.p1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.p1.Image = global::finger.Properties.Resources.Безымянный_1_02;
+            this.p1.Location = new System.Drawing.Point(590, 310);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(200, 200);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p1.TabIndex = 1;
+            this.p1.TabStop = false;
+            // 
+            // first
+            // 
+            this.first.BackColor = System.Drawing.Color.Transparent;
+            this.first.Image = global::finger.Properties.Resources.Безымянный_1_Монтажная_область_1;
+            this.first.Location = new System.Drawing.Point(525, 318);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(250, 250);
+            this.first.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.first.TabIndex = 0;
+            this.first.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Location = new System.Drawing.Point(1, 742);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(287, 100);
+            this.panel1.TabIndex = 5;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(3, 12);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(281, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1424, 841);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.last);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.first);
+            this.Name = "Form1";
+            this.Text = "Палец";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.last)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.first)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox first;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox last;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ finger/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace finger
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] pictureBoxes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            last.Location = MousePosition;
+            int x = (this.Width - first.Width) / 2;
+            int y = (this.Height - first.Height) / 2;
+            first.Location = new Point(x, y);
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Capture = true;
+            Point clientPoint = PointToClient(MousePosition);
+            last.Location = new Point(clientPoint.X - last.Width / 2, clientPoint.Y - last.Height / 2);
+            int p2x = (Convert.ToInt32(clientPoint.X - last.Width / 2) + Convert.ToInt32(first.Location.X)) / 2;
+            int p2y = (Convert.ToInt32(clientPoint.Y - last.Height / 2) + Convert.ToInt32(first.Location.Y)) / 2;
+            int p1x = (p2x + Convert.ToInt32(first.Location.X)) / 2;
+            int p1y = (p2y + Convert.ToInt32(first.Location.Y)) / 2;
+            int p3x = (Convert.ToInt32(clientPoint.X - last.Width / 2) + p2x) / 2;
+            int p3y = (Convert.ToInt32(clientPoint.Y - last.Height / 2) + p2y) / 2;
+            p1.Location = new Point(p1x, p1y);
+            p2.Location = new Point(p2x, p2y);
+            p3.Location = new Point(p3x, p3y);
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            this.Capture = false;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            int x = (this.Width - first.Width) / 2;
+            int y = (this.Height - first.Height) / 2;
+            first.Location = new Point(x, y);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            foreach (Control control in Controls)
+            {
+                if (control is PictureBox && control.Name != "first" && control.Name != "last")
+                {
+                    Controls.Remove(control);
+                    control.Dispose();
+                }
+            }
+            foreach (Control control in Controls)
+            {
+                if (control is PictureBox && control.Name != "first" && control.Name != "last")
+                {
+                    Controls.Remove(control);
+                    control.Dispose();
+                }
+            }
+
+
+        }
+    }
+}
